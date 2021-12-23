bae05e304b9a0e1ba2d440294f81fa1c92aadab0
Bryan Lee <bryanmylee@gmail.com>
2021-12-24 01:44:44 +0800

    feat: header hero title

---
+++ src/lib/home/components/Header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<section class="bg-gradient-to-b from-pink/50">
+	<header class="max-w-screen-md mx-auto p-8">
+		<h1 class="text-6xl font-bold drop-shadow-lg text-gradient mt-32">Techtris</h1>
+		<p>T-spinning since 2019</p>
+	</header>
+</section>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,31 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				purple: {
+					DEFAULT: 'rgb(169 146 207)',
+					dark: 'rgb(126 112 188)',
+				},
+				pink: {
+					DEFAULT: 'rgb(237 146 197)',
+				},
+				salmon: {
+					DEFAULT: 'rgb(223 130 140)',
+					light: 'rgb(216 150 148)',
+				},
+				gold: {
+					DEFAULT: 'rgb(237 180 132)',
+					light: 'rgb(243 199 127)',
+				},
+			},
+			fontFamily: {
+				sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
 
 	plugins: [],
