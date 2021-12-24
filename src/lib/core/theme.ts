a4badb9dfd20a9ccc79b33ef7808c1f7bfb3273d
Bryan Lee <bryanmylee@gmail.com>
2021-12-24 15:14:15 +0800

    feat: add 3d cube to header

---
+++ src/lib/core/theme.ts
@@ -0,0 +1,17 @@
+export const colors = {
+	purple: {
+		DEFAULT: 'rgb(169 146 207)',
+		dark: 'rgb(126 112 188)',
+	},
+	pink: {
+		DEFAULT: 'rgb(237 146 197)',
+	},
+	salmon: {
+		DEFAULT: 'rgb(223 130 140)',
+		light: 'rgb(216 150 148)',
+	},
+	gold: {
+		DEFAULT: 'rgb(237 180 132)',
+		light: 'rgb(243 199 127)',
+	},
+};
--- src/lib/home/components/Header.svelte
+++ src/lib/home/components/Header.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
+	import * as THREE from 'three';
+	import * as SC from 'svelte-cubed';
 </script>
 
-<section class="bg-gradient-to-b from-pink/50">
-	<header class="max-w-screen-md mx-auto p-8">
-		<h1 class="text-6xl font-bold drop-shadow-lg text-gradient mt-32">Techtris</h1>
+<section class="relative bg-gradient-to-b from-pink/50">
+	<header class="max-w-screen-md p-8 mx-auto">
+		<h1 class="mt-32 text-6xl font-bold drop-shadow-lg text-gradient">Techtris</h1>
 		<p>T-spinning since 2019</p>
 	</header>
+	<div class="absolute inset-0 left-1/2">
+		<SC.Canvas alpha antialias>
+			<SC.Mesh
+				geometry={new THREE.BoxGeometry()}
+				material={new THREE.MeshStandardMaterial({ color: 0xff3e00 })}
+			/>
+			<SC.PerspectiveCamera position={[1, 1, 3]} />
+			<SC.AmbientLight intensity={0.6} />
+			<SC.DirectionalLight intensity={0.6} position={[-2, 3, 2]} />
+		</SC.Canvas>
+	</div>
 </section>
