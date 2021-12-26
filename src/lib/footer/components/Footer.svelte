eeaf059c6b5211cdddec34968e1397e89b4fbd1d
Bryan Lee <bryanmylee@gmail.com>
2021-12-27 01:52:36 +0800

    feat: clearer header and footer text

--- src/lib/footer/components/Footer.svelte
+++ src/lib/footer/components/Footer.svelte
@@ -9,8 +9,8 @@
 				<FooterModelCanvas />
 			</div>
 		</div>
-		<p class="text-center w-full p-8 pb-16">
-			Developed by <strong>Team Techtris</strong>.
+		<p class="text-center w-full p-8 pb-16 text-xl">
+			Developed by <strong class="text-gradient">Team Techtris</strong>.
 		</p>
 	</div>
 </footer>
--- src/lib/home/components/Header.svelte
+++ src/lib/home/components/Header.svelte
@@ -4,10 +4,10 @@
 
 <section class="relative bg-gradient-to-b from-pink/50 aspect-video">
 	<header class="flex flex-col justify-end h-full max-w-screen-md gap-4 p-8 pb-12 mx-auto">
-		<h1 class="text-6xl font-bold">
+		<h1 class="text-6xl font-bold drop-shadow-lg">
 			<span>Tech</span><span class="text-salmon-300">tris</span>
 		</h1>
-		<p class="text-2xl font-bold text-pink-200">T-spinning since 2019</p>
+		<p class="text-2xl font-bold text-pink-200 drop-shadow-lg">T-spinning since 2019</p>
 	</header>
 	<div class="absolute inset-0 left-1/2 md:left-1/3 z-[-1]">
 		<TechtrisModelCanvas />
