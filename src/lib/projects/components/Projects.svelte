00ffa02293d62e3b1006d52a4fd4cf56e372cefb
Bryan Lee <bryanmylee@gmail.com>
2021-12-27 02:23:04 +0800

    style: purple bg

--- src/lib/projects/components/Projects.svelte
+++ src/lib/projects/components/Projects.svelte
@@ -5,7 +5,7 @@
 	import WokNRoll from './WokNRoll.svelte';
 </script>
 
-<section class="overflow-hidden bg-gradient-to-b from-slate-800 to-pink/50">
+<section class="overflow-hidden bg-gradient-to-b from-purple-400 to-pink/50">
 	<div class="flex flex-col max-w-screen-md gap-4 p-8 mx-auto">
 		<h2 class="text-4xl font-bold tracking-wide text-gradient">Our Projects</h2>
 		<HelloWorld />
