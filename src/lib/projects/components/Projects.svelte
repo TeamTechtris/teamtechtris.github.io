fe012e065deeae02703091084fd2c19d85a299d6
Bryan Lee <bryanmylee@gmail.com>
2021-12-27 04:12:56 +0800

    feat: page between project items

--- src/lib/projects/components/Projects.svelte
+++ src/lib/projects/components/Projects.svelte
@@ -1,16 +1,101 @@
+<script lang="ts" context="module">
+	const getPrevIndex = (currIndex: number, length: number): number => {
+		return (currIndex + length - 1) % length;
+	};
+	const getNextIndex = (currIndex: number, length: number): number => {
+		return (currIndex + 1) % length;
+	};
+</script>
+
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	import ComoClub from './ComoClub.svelte';
 	import HelloWorld from './HelloWorld.svelte';
 	import KopiTime from './KopiTime.svelte';
 	import WokNRoll from './WokNRoll.svelte';
+
+	let duration = 0;
+
+	let currentIndex = 0;
+	let jsEnabled = false;
+	onMount(() => {
+		jsEnabled = true;
+		setTimeout(() => {
+			duration = 500;
+		}, 20);
+	});
+
+	const pages = [HelloWorld, KopiTime, ComoClub, WokNRoll];
+
+	const handlePrev = () => {
+		currentIndex = getPrevIndex(currentIndex, pages.length);
+	};
+	const handleNext = () => {
+		currentIndex = getNextIndex(currentIndex, pages.length);
+	};
 </script>
 
 <section class="overflow-hidden bg-gradient-to-b from-purple-400 to-pink/50">
 	<div class="flex flex-col max-w-screen-md gap-4 p-8 mx-auto">
 		<h2 class="text-4xl font-bold tracking-wide text-gradient">Our Projects</h2>
-		<HelloWorld />
-		<KopiTime />
-		<ComoClub />
-		<WokNRoll />
+		<div class:zStack={jsEnabled} class="relative" style="--duration: {duration}ms">
+			{#each pages as page, index (index)}
+				<div
+					class="page"
+					class:hideLeft={jsEnabled &&
+						index !== currentIndex &&
+						getNextIndex(index, pages.length) === currentIndex}
+					class:hideRight={jsEnabled &&
+						index !== currentIndex &&
+						getNextIndex(index, pages.length) !== currentIndex}
+				>
+					<svelte:component this={page} />
+				</div>
+			{/each}
+			{#if jsEnabled}
+				<button
+					on:click={handlePrev}
+					class="absolute left-0 top-1/2 -translate-x-1/2 -translate-y-1/2 button pageButton"
+				>
+					prev
+				</button>
+				<button
+					on:click={handleNext}
+					class="absolute right-0 top-1/2 translate-x-1/2 -translate-y-1/2 button pageButton"
+				>
+					next
+				</button>
+			{/if}
+		</div>
 	</div>
 </section>
+
+<style lang="postcss">
+	.zStack {
+		@apply grid grid-cols-1;
+	}
+
+	.zStack > :global(*) {
+		grid-column: 1 / 1;
+		grid-row: 1 / 1;
+	}
+
+	.page {
+		transition: opacity var(--duration) ease-out, transform var(--duration) ease-out;
+	}
+
+	.hideLeft {
+		@apply opacity-0 pointer-events-none;
+		transform: translateX(-200px) rotate(-3deg);
+	}
+
+	.hideRight {
+		@apply opacity-0 pointer-events-none;
+		transform: translateX(200px) rotate(3deg);
+	}
+
+	.pageButton {
+		@apply w-12 h-12;
+	}
+</style>
