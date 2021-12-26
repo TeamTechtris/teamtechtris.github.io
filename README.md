af894e98e1e36f99c1de3b6e1198e1d86d834c11
Bryan Lee <bryanmylee@gmail.com>
2021-12-27 04:58:59 +0800

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# techtris.xyz
-
-Built on SvelteKit. `npm i` and `npm run dev` to get started.
--- src/lib/typings/svelte.ts
+++
@@ -1,7 +0,0 @@
-export type Action<T> = (
-	node: HTMLElement,
-	params?: T
-) => {
-	update?: (newParams: T) => void;
-	destroy?: () => void;
-};
