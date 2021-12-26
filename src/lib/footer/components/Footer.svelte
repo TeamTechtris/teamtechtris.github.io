9dc3a4d6d358e9f8989688a68da984d2eced3be2
Bryan Lee <bryanmylee@gmail.com>
2021-12-27 01:50:46 +0800

    style: faded pink bg

--- src/lib/footer/components/Footer.svelte
+++ src/lib/footer/components/Footer.svelte
@@ -2,7 +2,7 @@
 	import FooterModelCanvas from './FooterModelCanvas.svelte';
 </script>
 
-<footer class="bg-pink">
+<footer class="bg-pink/50">
 	<div class="max-w-screen-md mx-auto">
 		<div class="relative h-24">
 			<div class="absolute inset-0">
